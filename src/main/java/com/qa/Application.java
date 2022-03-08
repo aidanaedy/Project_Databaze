2e993ce9fd8e48153691439d8aa92be8bdb50f2f
Aidan Aedy <aidanaedy@gmail.com>
2022-03-08 16:20:07 +0000

    starting to get the database working so time for a commit!

--- src/main/java/com/qa/Application.java
+++ src/main/java/com/qa/Application.java
@@ -1,16 +1,17 @@
 package com.qa;
 
 import java.io.IOException;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.qa.scrape.*;
+
+import com.qa.entrance.Entrance;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) throws IOException {
 		
-		Scrape.Scrape();
+		Entrance.entrance();
+		
 
 	}
 
--- src/main/java/com/qa/books/Books.java
+++ src/main/java/com/qa/books/Books.java
@@ -8,75 +8,67 @@ import javax.persistence.Id;
 public class Books {
 
 	// defining my variables for the database
+
 	@Id // is the PK of our table
+	@Column(name = "ID")
+	private Integer id;
 	@Column(name = "Title")
 	private String title;
-	
-	@Column(name = "Author")
-	private String author;
 	@Column(name = "Price")
 	private Double price;
 	@Column(name = "Stock")
 	private Boolean inStock;
 
-	//no args constructor
-	public Books() {}
-	
-	//full args constructor
-	public Books(String title, String author, Double price, Boolean inStock) {
+	// no args constructor
+	public Books() {
+	}
+
+	// full args constructor
+	public Books(String title, Integer id, Double price, Boolean inStock) {
 		super();
 		this.title = title;
-		this.author = author;
+		this.id = id;
 		this.price = price;
 		this.inStock = inStock;
 	}
 
-	
-	//getters and setters
+	// getters and setters
 	public String getTitle() {
 		return title;
 	}
-
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-
-	public String getAuthor() {
-		return author;
+	public Integer getId() {
+		return id;
 	}
 
-
-	public void setAuthor(String author) {
-		this.author = author;
+	public void setId(Integer id) {
+		this.id = id;
 	}
-
 
 	public Double getPrice() {
 		return price;
 	}
 
-
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
 
 	public Boolean getInStock() {
 		return inStock;
 	}
 
-
 	public void setInStock(Boolean inStock) {
 		this.inStock = inStock;
 	}
 
-	//toString
+	// toString
 	@Override
 	public String toString() {
-		return "Books [title=" + title + ", author=" + author + ", price=" + price + ", inStock=" + inStock + "]";
+		return "Books [title=" + title + ", id=" + id + ", price=" + price + ", inStock=" + inStock + "]";
 	}
-	
 
 }
